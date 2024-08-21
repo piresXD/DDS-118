b298f21343d9d1ebb0e4aff92e96b1c907d15525
Davi Almeida <davi.a.pires@aluno.senai.br>
2024-08-21 16:56:40 -0300

    aula9js

---
+++ aula9JS/script.js
@@ -0,0 +1,171 @@
+//alert("Davi de almeida pires bem vindo")
+
+/*
+//Console
+console.log("Olha A Mensagem")
+
+var texto = "Bizerra da silva"
+var numero = 1989
+var TheBest = true
+var nulo = null
+var indefinido 
+
+console.log(typeof(texto))
+console.log(typeof(numero))
+console.log(typeof(TheBest))
+console.log(typeof(nulo))
+console.log(typeof(indefinido))
+
+*/
+
+//operadores
+//Aritméticos: +-/*%
+// var a = 10
+// var b = 3
+
+// console.log("Soma:", a+b)
+// console.log("Subtração:",a-b)
+// console.log("Multiplicação:",a*b)
+// console.log("Divisão:",a/b)
+// console.log("Módulo:",a%b)
+
+// //Lógicos: &&, ||, !
+// var verdade = true
+// var mentira = false
+
+// console.log(verdade && mentira)
+// console.log(verdade || mentira)
+// console.log(verdade && !mentira)
+
+
+// //Relacionais >,<,>=,==,!=,===,!==
+// var a = 10
+// var b = 5
+// var c = "10"
+
+// console.log("Relacionais")
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a == b)
+// console.log(a != b)
+// console.log(a === c)
+// console.log(a !== c)
+
+
+//Condicionais
+//if
+// var estaVivo 
+// console.log(estaVivo)
+// if(estaVivo == true){
+//     console.log("Está vivo");
+// }
+// else if(estaVivo == false){
+//     console.log("Está morto");
+// }
+// else{
+//     console.log("Não tá nada")
+// }
+
+//Switc
+// var camisa = "preta"
+// switch(camisa){
+//     case "Azul":
+//         console.log("A camisa é azul")
+//     break
+//     case "Amarela":
+//         console.log("A camisa é amarela")
+//     break
+//     case "preta":
+//         console.log("A camisa é preta")
+//     break
+//     default:
+//         console.log("A camisa é sei lá")
+//     break
+// }
+
+//loops
+//For
+// for(var i = 0; i < 5; i++){
+//     console.log("Estoy ahi");
+// }
+//     console.log("Acabou o For");
+
+//while
+// var it = 1 
+// while(it < 10){
+//     console.log("Estou aqui 2-0 retorno");
+//     it++
+// }
+
+//função
+// function teste1(){
+//     console.log("Estou aqui na função 1");
+// }
+
+// function teste2(){
+//     console.log("Estou aqui na função 2");
+// }
+
+// teste1()
+
+
+// Interação com o usuário via prompt
+//  var resposta = prompt("Digite seu email")
+//  var resposta2 = prompt("Digite sua CPF")
+ 
+//  console.log(resposta, resposta2)
+
+// alert(resposta, resposta2)
+
+var num1 = prompt("Digite um numero:")
+var num2 = prompt("Digite outro numero:")
+var resultado = Number(num1) + Number(num2)
+
+console.log(resultado)
+
+alert(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
